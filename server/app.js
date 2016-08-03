ede818a41057c1a880544faef74f81e574829425
tsakirist <tsakiris@uth.gr>
2016-08-03 15:59:58 +0300

    Added cluster and redis v1.0

--- server/app.js
+++ server/app.js
@@ -3,39 +3,69 @@ const app = express();
 const http = require('http').Server(app);
 const io = new require('socket.io')(http);
 const path = require('path');
+const cluster = require('cluster');
+const redis = require('socket.io-redis');
+const numWorkers = require('os').cpus().length;
 const PORT = 3000;
 const clients = {};
 
-app.use(express.static(path.resolve(__dirname + '/../')));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname + '/index.html'));
-});
-
-io.on('connection', (socket) => {
-    socket.on('disconnect', () => {
-        const username = clients[socket.id];
-        if(username) {
-	        delete clients[socket.id];
-	        io.emit('userDc', username);
-        }
+if(cluster.isMaster) {
+    console.log('Master cluster setting up ' + numWorkers + ' workers...');
+    for(let i=0; i<numWorkers; i++) {
+        cluster.fork();
+    }
+    cluster.on('online', (worker) => {
+        console.log('Worker ' + worker.process.pid + ' is online');
     });
-    socket.on('newMsg', (msg) => {
-        console.log('Message:', msg);
-        const data = {username: clients[socket.id], msg: msg};
-        socket.broadcast.emit('client msg', data);   // Send msg to all clients except sender
+    cluster.on('exit', (worker, code, signal) => {
+        console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
+        console.log('Starting a new worker');
+        cluster.fork();
     });
-    socket.on('newUser', (msg) => {
-        clients[socket.id] = msg;
+}
+else {
+    io.adapter(redis({host: 'localhost', port: 6379}));
+
+    app.use(express.static(path.resolve(__dirname + '/../')));
+
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname + '/index.html'));
+    });
+
+    io.on('connection', (socket) => {
+        console.log('Worker id', process.pid);
+        sendActive(socket);
+        socket.on('disconnect', () => {
+            const username = clients[socket.id];
+            if (username) {
+                console.log(`Disconnected ${process.pid} ${username}`);
+                delete clients[socket.id];
+                io.emit('userDc', username);
+            }
+        });
+        socket.on('newMsg', (msg) => {
+            // console.log('Message:', msg);
+            console.log(`worker ${process.pid} message: ${msg}`);
+            const data = {username: clients[socket.id], msg: msg};
+            socket.broadcast.emit('client msg', data);   // Send msg to all clients except sender
+        });
+        socket.on('newUser', (name) => {
+            clients[socket.id] = name;
+            sendActive(io);
+        });
+    });
+
+    function sendActive(socket) {
+        // Send all the active names to the connected socket
         const names = [];
-        for(var x in clients) {
+        for (var x in clients) {
             names.push(clients[x]);
         }
         console.log(names);
-        io.emit('newConn', names);
-    });
-});
+        socket.emit('active', names);
+    }
 
-http.listen(PORT, () => {
-    console.log('Listening at:',PORT);
-});
+    http.listen(PORT, () => {
+        console.log('Listening at:', PORT);
+    });
+}
