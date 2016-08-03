96cfe8b04e4328bd8f0a91f971866d8009122a39
tsakirist <tsakiris@uth.gr>
2016-08-03 16:51:33 +0300

    message passing IPC and also need to fix client names
    Need to fix client names

--- server/app.js
+++ server/app.js
@@ -7,15 +7,25 @@ const cluster = require('cluster');
 const redis = require('socket.io-redis');
 const numWorkers = require('os').cpus().length;
 const PORT = 3000;
-const clients = {};
+let clients = {};
+
+
+//TODO need to check client name cause it appends to the html
+
 
 if(cluster.isMaster) {
     console.log('Master cluster setting up ' + numWorkers + ' workers...');
     for(let i=0; i<numWorkers; i++) {
-        cluster.fork();
+        const worker = cluster.fork();
     }
     cluster.on('online', (worker) => {
         console.log('Worker ' + worker.process.pid + ' is online');
+        //This way we can send messages from workers to master and vice versa
+        //TODO need to check pid of worker and send message to all others referring to active clients
+        worker.on('message', (msg) => {
+            console.log(`Received message from worker ${worker.process.pid} msg:`, msg);
+            worker.send({msgMaster: clients});
+        });
     });
     cluster.on('exit', (worker, code, signal) => {
         console.log('Worker ' + worker.process.pid + ' died with code: ' + code + ', and signal: ' + signal);
@@ -24,6 +34,10 @@ if(cluster.isMaster) {
     });
 }
 else {
+    process.on('message', (msg) => {
+        console.log(`Received msg from master`, msg);
+    });
+
     io.adapter(redis({host: 'localhost', port: 6379}));
 
     app.use(express.static(path.resolve(__dirname + '/../')));
@@ -44,14 +58,15 @@ else {
             }
         });
         socket.on('newMsg', (msg) => {
-            // console.log('Message:', msg);
             console.log(`worker ${process.pid} message: ${msg}`);
             const data = {username: clients[socket.id], msg: msg};
             socket.broadcast.emit('client msg', data);   // Send msg to all clients except sender
         });
         socket.on('newUser', (name) => {
             clients[socket.id] = name;
+            //TODO check already existent names in html and do smthing like += for sendActive
             sendActive(io);
+            sendNames();
         });
     });
 
@@ -61,8 +76,12 @@ else {
         for (var x in clients) {
             names.push(clients[x]);
         }
-        console.log(names);
+        console.log('Names:',names);
         socket.emit('active', names);
+    }
+
+    function sendNames() {
+        process.send({msgWorker: clients});
     }
 
     http.listen(PORT, () => {
