d75f83bfc7c39602a422b73df6b47c8e343bd15d
tsakirist <tsakiris@uth.gr>
2016-08-04 11:44:35 +0300

    Small fixes

--- server/app.js
+++ server/app.js
@@ -6,25 +6,28 @@ const path = require('path');
 const cluster = require('cluster');
 const redis = require('socket.io-redis');
 const numWorkers = require('os').cpus().length;
-const PORT = 3000;
+const PORT = 3000,
+    redisHost = 'localhost',
+    redisPort = 6379;
 let clients = {};
-
+//let clients = [];
 
 //TODO need to check client name cause it appends to the html
 
 
 if(cluster.isMaster) {
+
     console.log('Master cluster setting up ' + numWorkers + ' workers...');
     for(let i=0; i<numWorkers; i++) {
         const worker = cluster.fork();
     }
+
     cluster.on('online', (worker) => {
         console.log('Worker ' + worker.process.pid + ' is online');
-        //This way we can send messages from workers to master and vice versa
         //TODO need to check pid of worker and send message to all others referring to active clients
         worker.on('message', (msg) => {
             console.log(`Received message from worker ${worker.process.pid} msg:`, msg);
-            worker.send({msgMaster: clients});
+            //worker.send({msgMaster: clients});
         });
     });
     cluster.on('exit', (worker, code, signal) => {
@@ -38,7 +41,7 @@ else {
         console.log(`Received msg from master`, msg);
     });
 
-    io.adapter(redis({host: 'localhost', port: 6379}));
+    io.adapter(redis({host: redisHost, port: redisPort}));
 
     app.use(express.static(path.resolve(__dirname + '/../')));
 
@@ -47,8 +50,9 @@ else {
     });
 
     io.on('connection', (socket) => {
-        console.log('Worker id', process.pid);
-        sendActive(socket);
+        console.log('Connected Worker id', process.pid);
+        //sendActive(socket);
+
         socket.on('disconnect', () => {
             const username = clients[socket.id];
             if (username) {
@@ -60,24 +64,19 @@ else {
         socket.on('newMsg', (msg) => {
             console.log(`worker ${process.pid} message: ${msg}`);
             const data = {username: clients[socket.id], msg: msg};
-            socket.broadcast.emit('client msg', data);   // Send msg to all clients except sender
+            socket.broadcast.emit('clientMsg', data); // Send message to everyone besides myself
         });
         socket.on('newUser', (name) => {
             clients[socket.id] = name;
-            //TODO check already existent names in html and do smthing like += for sendActive
-            sendActive(io);
+            console.log(`Worker ${process.pid} updated clients name`, name);
+            sendActive(socket, name);
             sendNames();
         });
     });
 
-    function sendActive(socket) {
-        // Send all the active names to the connected socket
-        const names = [];
-        for (var x in clients) {
-            names.push(clients[x]);
-        }
-        console.log('Names:',names);
-        socket.emit('active', names);
+    function sendActive(socket, name) {
+        console.log('Sending ', name);
+        socket.broadcast.emit('active', name);
     }
 
     function sendNames() {
